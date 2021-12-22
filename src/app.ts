90783ad090b0cec731759db83e645836e703b628
Ekaterina Shepeleva <ea.shepeleva95@gmail.com>
2021-12-22 15:49:53 +0300

    feat: add request logging

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import { PORT, HOST } from './config/config';
 import { userRoutes } from './resources/users/user.router';
 import { boardRoutes } from './resources/boards/board.router';
 import { taskRoutes } from './resources/tasks/task.router';
-import { loggerOpts } from './logging';
+import { addReqLogging, loggerOpts } from './logging';
 
 const server = createServer(loggerOpts);
+
+addReqLogging(server);
 
 server.register(userRoutes);
 server.register(boardRoutes);
--- src/logging/index.ts
+++ src/logging/index.ts
@@ -1,8 +1,29 @@
-import { FastifyLoggerOptions } from 'fastify';
+import { FastifyInstance, FastifyLoggerOptions } from 'fastify';
 
 const loggerOpts = {
-  logger: true,
+  logger: {
+    level: 'info',
+    prettyPrint: {
+      colorize: false,
+      ignore: 'time,pid,hostname,reqId',
+      singleLine: true,
+    },
+  },
   disableRequestLogging: true,
 } as FastifyLoggerOptions;
 
-export { loggerOpts };
+function addReqLogging(server: FastifyInstance) {
+  server.addHook('preHandler', (req, _reply, done) => {
+    const { method, url, params, body, query } = req;
+
+    req.log.info(
+      `Received ${method} request to ${url} with params: ${JSON.stringify(
+        params
+      )}, query: ${JSON.stringify(query)}, body: ${JSON.stringify(body)} `
+    );
+
+    done();
+  });
+}
+
+export { loggerOpts, addReqLogging };
